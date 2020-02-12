8bfde7993fee9b2074ada0a27c1873b86cd99678
Steven Riggs <sfriggs21@gmail.com>
2020-02-12 16:22:52 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,13 @@
 def oxford_comma(array)
-
+  len = array.length
+  case len
+  when 1
+    array[0]
+  when 2
+    array.join(" and ")
+  else
+    front = array[0..-2].join(", ")
+    back = array[-1]
+    front + ", and " + back
+  end
 end
