c6ab456995a02cf48cdb35ab8ef65974b4819929
Frank  D'Angelo <08fdange@gmail.com>
2020-02-14 17:42:02 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,13 @@
 def oxford_comma(array)
-
+  string = ""
+  if array.length == 1 
+    string << array.join
+  elsif array.length == 2
+    string << array.join(" and ")
+  elsif array.length >= 3
+    last_array_item = array.pop
+    string << array.join(", ")
+    string << ", and " + last_array_item
+  end
+  return string
 end
