10df4533ca2d0fa8e6f5c4f9af842262b1f8ca27
Malcom Fetus <sora.vista@gmail.com>
2020-02-19 19:09:05 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
+require 'pry'
 def oxford_comma(array)
-
+  #binding.pry
+if array.length == 1
+  array.join()
+elsif array.length == 2
+array.join( " and ")
+else
+    return array[0..-2].join(", ") + ", and " + array[-1]
+  end
 end
